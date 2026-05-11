b4d7649bdfcc9dee3acc52f68b83b9a37feec017
Dzara Melcone <dzaramelcone@gmail.com>
2026-05-11 06:30:51 -0400

    test

--- src/test.zig
+++ src/test.zig
@@ -7,6 +7,7 @@ test {
     std.testing.refAllDecls(engine.images);
     std.testing.refAllDecls(engine.asset.mesh);
     std.testing.refAllDecls(engine.gpu.api);
+    std.testing.refAllDecls(engine.gpu.Api);
     std.testing.refAllDecls(engine.render.gpu_scene);
     std.testing.refAllDecls(project.scene);
     std.testing.refAllDecls(cook.images);
